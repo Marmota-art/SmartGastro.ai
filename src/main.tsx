2f22f26d1840aa4b391a81e9fa7e0369ccfefbf3
Marmota-art <loewenburg@bluewin.ch>
2026-03-01 13:45:08 +0100

    MIME type

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-console.log('SmartGastro.ai initializing...');
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
